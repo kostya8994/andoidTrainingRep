c05759a9037f16f6a910236d8e2a296ae07173f8
kostya8994 <kostya8994@yandex.ru>
2022-07-24 19:49:18 +0800

    Четвертое задание

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.post)
+
+        val likeButton = findViewById<ImageButton>(R.id.licksButton)
+        val likeText = findViewById<TextView>(R.id.licksQuantity)
+        val shareButton = findViewById<ImageButton>(R.id.shareButton)
+        val shareText = findViewById<TextView>(R.id.shareQuantity)
+        val post = Post(false, "999", "0")
+        val postLogic = PostLogic(post)
+        likeButton.setOnClickListener{
+            if(!post.likedByMy)likeButton.setImageResource(R.drawable.ic_licksred_24) else likeButton.setImageResource(R.drawable.ic_lickes_24dp)
+            if(!post.likedByMy) postLogic.clickLike() else postLogic.cancelingClickLike()
+            likeText.setText(post.numberLickes)
+            post.likedByMy = !post.likedByMy
+        }
+        shareButton.setOnClickListener{
+            postLogic.clickShare()
+            shareText.setText(post.numberShare)
+        }
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -0,0 +1,7 @@
+package ru.netology.nmedia
+
+data class Post(
+    var likedByMy: Boolean = false,
+    var numberLickes: String = "0",
+    var numberShare: String = "0"
+)
